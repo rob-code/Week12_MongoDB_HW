eaabd41c88478424ea62c030f7b793e5cd04b1ae
Robert Brice <rob.a.brice@gmail.comC>
2017-04-18 15:48:54 +0100

    add database seeds

---
+++ seeds.js
@@ -0,0 +1,25 @@
+use ratings_site;
+
+db.yachts.insert([
+  { 
+    name: "Querida",
+    type: "Trapper 400",
+    length: "28'",
+    engine: "12hp",
+    review: "Great value for budget family cruising"
+  },
+  { 
+    name: "J-Berry",
+    type: "J24",
+    length: "24'",
+    engine: "none - outboard required",
+    review: "Solid, proven inshore racer."
+  },
+  {
+    name: "Riot",
+    type: "Etchell-22",
+    length: "22'",
+    engine: "none - outboard required",
+    review: "Fast thoroughbred racer. Sharp performance curve means to win races you really need to know what you are doing."
+  }
+  ]);
